9bd312c4fb9fd34b8dc7b4d337f9e88c57027ee6
Chris Poirier <xxxchrispoirierxxx@gmail.com>
2016-05-06 22:50:33 -0400

    renamed install.sh to install_vimrc.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-Remove source config files and create symbolic links with the following command
-
-#FOR vim configuration file
-# rm ~/.vimrc
-ln -s /absolute/path/.vimrc ~/
-vim
-:PluginInstall
-:BundleInstall
-
